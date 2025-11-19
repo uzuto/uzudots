ff4e1703d92c1cb919b1b5b28619360e18b34550
uzuto <uzuto.git@gmail.com>
2025-11-19 18:16:12 +0530

    Init

---
+++ bspwm/.config/bspwm/rofi/launcher-sp/launcher.sh
@@ -0,0 +1,5 @@
+dir="$HOME/.config/bspwm/rofi/launcher-sp/"
+## Run
+rofi \
+    -show drun \
+    -theme ${dir}/spl.rasi
---
+++ geany/.config/geany/filedefs/filetypes.cpp
@@ -0,0 +1,7 @@
+[build-menu]
+EX_00_LB=_Execute
+EX_00_CM="./%e"
+EX_00_WD=
+EX_01_LB=
+EX_01_CM=
+EX_01_WD=
